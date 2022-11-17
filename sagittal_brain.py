09ab232517af5c08724b78a8b2f2d3c692f4d114
Bronte Wan <bronte288@hotmail.com>
2022-11-17 04:35:08 +0000

    generates input that causes error

--- sagittal_brain.py
+++ sagittal_brain.py
@@ -2,6 +2,10 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 import numpy as np
 
+data_input = np.zeros((20, 20))
+data_input[-1, :] = 1
+np.savetxt("brain_sample.csv", data_input, fmt='%d', delimiter=',')
+
 
 def run_averages(file_input='brain_sample.csv', file_output='brain_average.csv'):
     """
