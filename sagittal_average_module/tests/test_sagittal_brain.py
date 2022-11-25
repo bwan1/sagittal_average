54301bd907e42232be9e378563acf56740641207
Bronte Wan <bronte288@hotmail.com>
2022-11-25 02:51:48 +0000

    test and __init__.py added

---
+++ sagittal_average_module/tests/test_sagittal_brain.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sagittal_brain import *
+
+data_input = np.zeros((20, 20))
+data_input[-1, :] = 1
+
+np.savetxt("brain_sample.csv", data_input, fmt='%d', delimiter=',')
+
+# The expeted result is all zeros, except the last one, it should be 1
+expected = np.zeros(20)
+expected[-1] = 1
+
+run_averages(file_input="brain_sample.csv",
+             file_output="brain_average.csv")
+
+result = np.loadtxt("brain_average.csv",  delimiter=',')
+
+np.testing.assert_array_equal(result, expected)
